28d5fb2f113b1dd8d2e7dc55cc640afd44cca811
welpodron <welpodron@gmail.com>
2024-01-18 21:52:49 +0300

    Перевод скриптов сборщика в пакет + первая итерация тестов

---
+++ install/packages/welpodron.feedback/README.md
@@ -0,0 +1,9 @@
+# Пакет модуля welpodron.feedback
+
+Данный пакет содержит стили и скрипты входящие в модуль welpodron.feedback 
+
+**Текущая версия документации: https://welpodron-docs.ru/**
+
+Старая версия документации: https://welpodron.github.io/web-components/
+
+> @welpodron 2021-2023
---
+++ install/packages/welpodron.feedback/ts/form/index.test.ts
@@ -0,0 +1,38 @@
+import { formFeedback } from '.';
+
+describe('formFeedback', () => {
+  //! TODO: Добавить тесты мока для fetch
+  it('Форма должна быть заблокирована для отправки при инициализации компонента, если присутствуют невалидные поля', () => {
+    // INITIAL SETUP
+    document.body.innerHTML = `<form>
+        <input required />
+        <button type="submit">Кнопка отправки с указанием type</button>
+        <button>Кнопка отправки без указания type</button>
+        <button type="reset">Кнопка сброса</button>
+        <button type="button">Обычная кнопка</button>
+        <input type="submit" value="Кнопка через input" />
+    </form>`;
+
+    const instance = new formFeedback({
+      element: document.forms[0],
+    });
+
+    document.forms[0].querySelectorAll('input').forEach((input) => {
+      if (input.type === 'submit') {
+        expect(input).toBeDisabled();
+      } else if (!input.hasAttribute('disabled')) {
+        expect(input).not.toBeDisabled();
+      }
+    });
+
+    document.forms[0].querySelectorAll('button').forEach((button) => {
+      if (button.type === 'submit' || !button.hasAttribute('type')) {
+        expect(button).toBeDisabled();
+      } else {
+        expect(button).not.toBeDisabled();
+      }
+    });
+
+    //! TODO: Добавить обработку нажатия кнопки и тд
+  });
+});
